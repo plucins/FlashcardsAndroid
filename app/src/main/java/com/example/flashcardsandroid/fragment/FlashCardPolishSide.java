cb37e3c9ddad39ea3a1d11aef7d69339e4aa9867
Adam <s15814@pjwstk.edu.pl>
2020-05-16 22:03:54 +0200

    Make flashcard display data from DataProvider
    Need to figure out how to pass flashcard number from activity

--- app/src/main/java/com/example/flashcardsandroid/fragment/FlashCardPolishSide.java
+++ app/src/main/java/com/example/flashcardsandroid/fragment/FlashCardPolishSide.java
@@ -4,14 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.flashcardsandroid.DataProvider;
 import com.example.flashcardsandroid.R;
+import com.example.flashcardsandroid.model.FlashCard;
+
+import java.util.List;
 
 public class FlashCardPolishSide extends Fragment {
+    DataProvider provider;
+    String polishText = "Empty";
+    List<FlashCard> flashCards;
+    Button polishButton;
+    int flashCardNumber = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.polish_flash_card_side, container, false);
@@ -20,6 +31,13 @@ public class FlashCardPolishSide extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        provider = new DataProvider();
+        flashCards = provider.getFlashCards();
+        polishText = flashCards.get(flashCardNumber).getPolishSide();
+        polishButton = view.findViewById(R.id.polishButton);
+        polishButton.setText(polishText);
+
 
         view.findViewById(R.id.polishButton).setOnClickListener(new View.OnClickListener() {
             @Override
