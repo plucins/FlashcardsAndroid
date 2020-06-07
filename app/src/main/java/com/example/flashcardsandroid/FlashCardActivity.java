d9e88daf478c59f9b092df1efaf769862cca206d
Adam <s15814@pjwstk.edu.pl>
2020-06-07 13:38:47 +0200

    Add flash card animation

--- app/src/main/java/com/example/flashcardsandroid/FlashCardActivity.java
+++ app/src/main/java/com/example/flashcardsandroid/FlashCardActivity.java
@@ -1,35 +1,95 @@
 package com.example.flashcardsandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FlashCardActivity extends AppCompatActivity {
 
+    private AnimatorSet mSetRightOut;
+    private AnimatorSet mSetLeftIn;
+    private boolean mIsBackVisible = false;
+    private View mCardFrontLayout;
+    private View mCardBackLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card);
+        findViews();
+        loadAnimations();
+        changeCameraDistance();
 
-        Button backToMenu = findViewById(R.id.flashCardBackToMenuButton);
-        Button nextFlashCard = findViewById(R.id.nextFlashCardButton);
-
-        backToMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                backToMenu();
-            }
-        });
     }
 
-    private void backToMenu() {
-        Intent intent = new Intent(FlashCardActivity.this, StartingScreenActivity.class);
-        startActivity(intent);
+    private void changeCameraDistance() {
+        int distance = 8000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mCardFrontLayout.setCameraDistance(scale);
+        mCardBackLayout.setCameraDistance(scale);
     }
 
+    private void loadAnimations() {
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.out_animation);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.in_animation);
+    }
 
+    private void findViews() {
+        mCardBackLayout = findViewById(R.id.card_back);
+        mCardFrontLayout = findViewById(R.id.card_front);
+    }
+
+    public void flipCard(View view) {
+        if (!mIsBackVisible) {
+            mSetRightOut.setTarget(mCardFrontLayout);
+            mSetLeftIn.setTarget(mCardBackLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = true;
+        } else {
+            mSetRightOut.setTarget(mCardBackLayout);
+            mSetLeftIn.setTarget(mCardFrontLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = false;
+        }
+    }
 }
+//package com.example.flashcardsandroid;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+//public class FlashCardActivity extends AppCompatActivity {
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.flash_card);
+//
+//        Button backToMenu = findViewById(R.id.flashCardBackToMenuButton);
+//        Button nextFlashCard = findViewById(R.id.nextFlashCardButton);
+//
+//        backToMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                backToMenu();
+//            }
+//        });
+//    }
+//
+//    private void backToMenu() {
+//        Intent intent = new Intent(FlashCardActivity.this, StartingScreenActivity.class);
+//        startActivity(intent);
+//    }
+//
+//
+//}
